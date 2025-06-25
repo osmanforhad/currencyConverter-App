33967b824edb3366c537b386e7139bbd2d3a5597
osmanforhad <osmanforhad93@gmail.com>
2025-06-25 22:15:42 +0600

    design buttion component and setup currency consts with an interface

--- source/App.tsx
+++ source/App.tsx
@@ -5,24 +5,23 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+
 });
 
 export default App;
